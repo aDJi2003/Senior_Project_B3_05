cb58e4a2917b751e38290999353e69489046b00e
Venamora <stevennamorarohasaraginapitu@mail.ugm.ac.id>
2025-05-09 02:58:26 +0700

    feat: routes

--- backend/src/routes/sampahRoutes.js
+++ backend/src/routes/sampahRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const sampahController = require("../controllers/sampahController");
 const authMiddleware = require("../middleware/authMiddleware");
+const { getTotalWaste } = require("../controllers/sampahController");
 
 // Perbaiki rute
 router.get("/", sampahController.getAllSampah);
-// router.get("/by-email", authMiddleware, sampahController.getSampahByEmail); 
+router.get('/weekly-weight', sampahController.getWeeklyWeight);
+router.get("/total-waste", getTotalWaste);
 router.get("/:id", authMiddleware, sampahController.getSampahByUserId);
 router.post("/create", authMiddleware, sampahController.createSampah);
 router.put("/:id", sampahController.updateSampah);
 router.delete("/:id", sampahController.deleteSampah);
-console.log(sampahController); 
-module.exports = router; 
+
+module.exports = router;
 
